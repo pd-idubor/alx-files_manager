8da04948db8de16a5fe5db3fb9f86a3ef0a94a74
Precious Idubor <preciousidubor@gmail.com>
2023-09-06 10:17:07 -0700

    RedisClient

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
+  }
+
+  async set(key, value, timeout) {
+    const asyncSet = promisify(this.client.set).bind(this.client);
+    await asyncSet(key, value);
+    this.client.expire(key, timeout);
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    await asyncDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
